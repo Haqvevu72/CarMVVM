da7584d8b3f65948256e9128a72672e9d3b24ea4
Haqve_vu72 <Haqve_vu72@itstep.edu.az>
2023-07-13 07:45:39 +0400

    Add project files.

---
+++ WpfApp1/Model/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    internal class Car
+    {
+        public string? Image { get; set; }
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public string? Year { get; set; }
+        
+        public Car(string? image, string? make, string? model, string? year)
+        {
+            Image = image;
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+    }
+}
---
+++ WpfApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    public  class MainViewModel
+    {
+
+        public MainViewModel() 
+        {
+            
+        }
+
+    }
+}
